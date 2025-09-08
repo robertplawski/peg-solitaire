1e40e118aa7ede2f9f92593245d38d8efb313674
Robert Plawski <dev@robertplawski.pl>
2025-09-09 00:31:38 +0200

    Refactored, added button icons, styled and whatnot

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // TODO please create a layer of separation between DOM and game logic - to increase performance
 
-
-// init vars
-let selectedPeg, moves;
+let selectedPeg;
 
 // dashboard buttons...
+
+const rotateBoard = (degrees) => {
+  board.style.rotate = `${parseInt(board.style.rotate.slice(0, -3) || 0) + degrees}deg`;
+}
+
+rotateLeftButton.onclick = () => rotateBoard(90)
+rotateRightButton.onclick = () => rotateBoard(-90)
+
+resetButton.onclick = () => init();
 undoButton.onclick = () => {
   const popped = moves.pop();
   if (!popped) {
@@ -14,27 +21,13 @@ undoButton.onclick = () => {
   makeMove(target, center, origin, true)
 }
 
-resetButton.onclick = () => {
-  init();
-}
-
-rotateLeftButton.onclick = () => {
-  board.style.rotate = `${parseInt(board.style.rotate.slice(0, -3) || 0) + 90}deg`;
-}
-rotateRightButton.onclick = () => {
-  board.style.rotate = `${parseInt(board.style.rotate.slice(0, -3) || 0) - 90}deg`;
-}
-
-const hidePossibleMoves = () => {
-  Array.from(document.querySelectorAll(".possible")).forEach((el) => el.classList.remove("possible"))
-}
+const hidePossibleMoves = () => Array.from(document.querySelectorAll(".possible")).forEach((el) => el.classList.remove("possible"))
 
 const getPegPos = (peg) => {
   return { x: parseInt(peg.getAttribute("x")), y: parseInt(peg.getAttribute("y")) };
 }
 
 const makeMove = (origin, center, target, undo = false) => {
-
   if (!undo && origin != selectedPeg) {
     return;
   }
@@ -42,13 +35,11 @@ const makeMove = (origin, center, target, undo = false) => {
     moves.push({ origin, center, target });
   }
   hidePossibleMoves();
+
   origin.classList.remove("on");
-  if (!undo) {
-    center.classList.remove("on");
-  } else {
-    center.classList.add("on");
-  }
   target.classList.add("on");
+  undo ? center.classList.add("on") : center.classList.remove("on")
+
   calculateMoveCount();
 }
 
@@ -56,9 +47,13 @@ const makeMove = (origin, center, target, undo = false) => {
 const calculateMovesAtPosition = (x, y) => {
   const offsets = [-2, 2];
   return [
-    offsets.map((offset) => { return { origin: getPegAt(x, y), center: getPegAt(x + parseInt(offset / 2), y), target: getPegAt(x + offset, y) } }),
+    offsets.map((offset) => { return { origin: getPegAt(x, y), center: getPegAt(x + offset / 2, y), target: getPegAt(x + offset, y) } }),
     offsets.map((offset) => { return { origin: getPegAt(x, y), center: getPegAt(x, y + offset / 2), target: getPegAt(x, y + offset) } })
-  ].flat().filter((move) => move.origin && move.origin.classList.contains("on") && move.center && move.target && move.center.classList.contains("on") && !move.target.classList.contains("on"))
+  ]
+    .flat()
+    .filter(({ origin, center, target }) => origin && origin.classList.contains("on") &&
+      center && center.classList.contains("on") &&
+      target && !target.classList.contains("on"))
 }
 
 
@@ -90,22 +85,22 @@ const getPegAt = (x, y) => {
 
 const calculateMoveCount = () => {
   let pegCount = 0;
-  let result = 0;
+  let possibleMovesCount = 0;
   Array.from(board.children).forEach((row) => {
     Array.from(row.children).forEach((peg) => {
       if (peg.classList.contains("on")) {
         pegCount++;
       }
       const { x, y } = getPegPos(peg)
-      result += calculateMovesAtPosition(x, y).length
+      possibleMovesCount += calculateMovesAtPosition(x, y).length
     })
   })
 
+  pegCountSpan.innerText = pegCount;
+  possibleMovesCountSpan.innerText = possibleMovesCount;
 
-  moveCount.innerText = `possible moves: ${result}, peg count: ${pegCount}`;
-  if (result != 0) {
+  if (possibleMovesCount != 0) {
     return;
-
   }
   if (pegCount > 1) {
     alert("Przegraleś...");
@@ -119,13 +114,10 @@ const init = () => {
   selectedPeg = null;
   moves = [];
 
-  let rowHeight = 2;
-  let rowLength = 2;
-  let i = 0;
   for (let j = 0; j < 3; j++) {
-    let special = (j == 1)
-    rowHeight = special ? 3 : 2;
-    rowLength = special ? 7 : 3;
+    let center = (j == 1)
+    rowHeight = center ? 3 : 2;
+    rowLength = center ? 7 : 3;
     for (let y = 0; y < rowHeight; y++) {
       let row = document.createElement("div")
       row.classList.add("row")
@@ -134,10 +126,16 @@ const init = () => {
         let peg = document.createElement("div")
         peg.classList.add("peg")
         peg.classList.add("on");
-        peg.setAttribute('x', x - (special ? 2 : 0))
+
+        // if center then do some magic so the x pos makes sense
+        // its relative to center, so pegs at the left side have negative x pos 
+        peg.setAttribute('x', x - (center ? 2 : 0))
+        // a lot of magic numbers - all you need to know is that it works
         peg.setAttribute('y', y + j + (j > 0 ? 1 : 0) + (j > 1 ? 2 : 0));
+
+        peg.addEventListener("click", () => setSelectedPeg(peg));
+
         row.appendChild(peg)
-        peg.addEventListener("click", () => setSelectedPeg(peg)); i++;
       }
       board.appendChild(row)
     }
@@ -148,5 +146,4 @@ const init = () => {
 
   calculateMoveCount()
 }
-
-window.addEventListener("load", init)
+init()
