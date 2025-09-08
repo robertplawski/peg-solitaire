c0bb0f0e72763a76a52ab2c963cf52975aa9675f
Robert Plawski <e@rplawski.pl>
2025-09-08 19:32:42 +0200

    Added basic WIP peg selection system, bug in the coordinate system though

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const hidePossibleMoves = () => {
 	Array.from(document.querySelectorAll(".possible")).forEach((el)=>el.classList.remove("possible"))}
 
 const getPegCoordinates = (peg) => {
-	return {x:peg.getAttribute("x"),y:peg.getAttribute("y")};
+	return {x:parseInt(peg.getAttribute("x")),y:parseInt(peg.getAttribute("y"))};
 }
 
 const showPossibleMoves = (peg) => {
 	const {x,y} = getPegCoordinates(peg);
 
+	// x+2 y+2 x-2 y-2
+
 	const possibleMoves = [];
+
+	const offsets = [2,-2];
+	
+	offsets.forEach(offset=>possibleMoves.push(getPegByCoordinates(x+offset,y)))
+	offsets.forEach(offset=>possibleMoves.push(getPegByCoordinates(x,y+offset)))
+	
+	console.log(possibleMoves)
+
+	possibleMoves.filter(el=>el!=null).filter(el=>!el.classList.contains("on")).forEach((el)=>el.classList.add("possible"))
 	
 }
 
@@ -26,8 +37,8 @@ const setSelectedPeg =(peg) =>{
 
 }
 
-const getPegByIndex = (index) => {
-	return	document.querySelector(`.peg[i='${index}']`)
+const getPegByCoordinates = (x,y) => {
+	return document.querySelector(`[x="${x}"][y="${y}"]`)
 }
 
 const init = () => {
@@ -46,8 +57,8 @@ const init = () => {
 				let peg = document.createElement("div")
 				peg.classList.add("peg")
 				peg.classList.add("on");
-				peg.setAttribute('x',x - (special ? 2 : 0));
-				peg.setAttribute('y',(j*(rowHeight + (j==2 ? 1 : 0)))+y);
+				peg.setAttribute('x',x - (special ? 2 : 0))
+				peg.setAttribute('y',(j*(rowHeight-1) + (j==2 ? 1 : 0)+y));
 				peg.setAttribute('i', `${peg.getAttribute('x')}:${peg.getAttribute('y')}`);
 				row.appendChild(peg)
 				peg.addEventListener("click",()=>setSelectedPeg(peg));				i++;
@@ -56,7 +67,7 @@ const init = () => {
 		}
 	}
 
-	getPegByCoordinates(1,4).classList.remove("on");
+	getPegByCoordinates(1,3).classList.remove("on");
 
 }
 
